1313d077afa670db4cc21bbaedd013aefacfaf3f
Jianer <congjianer@outlook.com>
2022-11-01 21:57:34 +0800

    Start doing menu

--- lrn/h5/react/my-vite-react-ts/src/App.tsx
+++ lrn/h5/react/my-vite-react-ts/src/App.tsx
@@ -1,32 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+interface menuItem {
+    name: string;
+    price: number;
+    count?: number;
+}
+
+
+function Menu(props) {
+    const menu : menuItem[] = [
+        {name: 'egg', price: 1.1},
+        {name: 'apple', price: 2.2},
+        {name: 'wheat', price: 3.3},
+        {name: 'tomato', price: 4.4},
+    ];
+
+    const items = menu.map(
+        (item: menuItem) => <div key={item.name}>{item.name} | {item.price}</div>
+    );
+
+    return (
+        <div>
+            {items}
+        </div>
+    );
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Menu/>
     </div>
   )
 }
