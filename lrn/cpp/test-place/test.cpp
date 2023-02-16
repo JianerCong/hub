dff86918426a6bb66bff68a4eaa3cf8d9a184772
Jianer <congjianer@outlook.com>
2023-02-16 16:02:46 +0800

    Add test place for cpp

---
+++ lrn/cpp/test-place/test.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <vector>
+
+
+int main(){
+//   for (int i = 0; i < 10; i++){
+//     printf("%d / 2 is %d\n", i, i / 2);
+// }
+  for (int i : {1,2,3}){        // include <vector>
+    printf("%d",i);
+}
+}
