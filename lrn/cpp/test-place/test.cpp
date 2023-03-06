c542ac4fcfff70a32b34df9ddc801c32f0b5bbd2
Jianer <congjianer@outlook.com>
2023-03-06 18:41:52 +0800

    Start learning c+20

--- lrn/cpp/test-place/test.cpp
+++ lrn/cpp/test-place/test.cpp
@@ -1,31 +1,14 @@
-// #define BOOST_TEST_MAIN
-#define BOOST_TEST_MODULE MyTest
-#include <boost/test/unit_test.hpp>
-#include <stdexcept>
+#include <iostream>
+#include <format>
+// #include <fmt/format.h>
 
+using std::cout;
+using std::string;
+// using fmt::format;
 
-BOOST_AUTO_TEST_CASE(test_1) {
-  BOOST_CHECK_NE(2,1);          // 2 != 1
-} // BOOST_AUTO_TEST_CASE(test_no_1)
+int main(){
+  string s{"aaa"};
+  int g{123};
 
-void f(){
-  throw std::runtime_error("hi");
-}
-
-BOOST_AUTO_TEST_CASE(test_2) {
-  BOOST_CHECK_THROW(f(), std::runtime_error);
-}
-
-
-BOOST_AUTO_TEST_CASE(test_equal) {
-  BOOST_CHECK_EQUAL(1, 1);
-}
-
-BOOST_AUTO_TEST_CASE(test_error) {
-  BOOST_ERROR("this should give error ❄");
-}
-
-
-BOOST_AUTO_TEST_CASE(test_fail) {
-  BOOST_FAIL("this should give fatal error ❄");
+  cout << format("aaa={},123={}\n",s,g);
 }
--- lrn/python/m.py
+++ lrn/python/m.py
@@ -1,13 +1,25 @@
-x = 1
+import asyncio
+from kademlia.network import Server
 
-class C():
-    x = globals()['x']
-    y = globals().get('y', None)
-    print(f"x is {x}")
-    print(f"y is {y}")
-    def __init__(self):
-        print(f"x is {C.x}")
-        print(f"y is {C.y}")
+async def run():
+    # Create a node and start listening on port 5678
+    node = Server()
+    node2 = Server()
 
-c = C()
-# x is 1
+    await node.listen(5678)
+    await node2.listen(5679)
+
+    # Bootstrap the node by connecting to other known nodes, in this case
+    # replace 123.123.123.123 with the IP of another node and optionally
+    # give as many ip/port combos as you can for other nodes.
+    await node.bootstrap([("localhost", 5679)])
+    # await node2.bootstrap([("localhost", 5678)])
+
+    # set a value for the key "my-key" on the network
+    await node.set("my-key", "my-value")
+
+    # get the value associated with "my-key" from the network
+    result = await node2.get("my-key")
+    print(f'Value from node2 is {result}')
+
+asyncio.run(run())
