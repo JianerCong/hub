85ae47a360994522012c89b05c3ca006e7ded38f
Jianer <congjianer@outlook.com>
2023-06-23 23:35:21 +0800

    Start writing follow-me consensus

--- lrn/LaTeX/test/scratch.py
+++ lrn/LaTeX/test/scratch.py
@@ -11,7 +11,7 @@ class IEndpointBasedNetworkable:
         pass
     def listened_endpoint() -> str:
         pass
-    def send(self,endpoint: str, target: str) -> str:
+    def send(self,endpoint: str, target: str, data: str) -> str:
         # endpoint: the target, e.g. "localhost:8080"
         # target: e.g. "/hi"
         pass
@@ -38,10 +38,62 @@ class ListenToOneConsensus:
             self.start_listening_as_primary()
 
     def ask_primary_for_entry(self):
-        r = self.n.send(self.primary,
+        r = self.net.send(self.primary,
                     '/pleaseAddMe',
                     self.n.listened_endpoint())
-        # In response, the primary may ask you to execute something.
-        if r:
-            self.exe.execute(r)
-    
+        # In response, the primary should send you the history .
+        cmd = r.split('\n')[3]  # forth line is the command
+        if cmd:
+            self.exe.execute(cmd)
+
+    def start_listening_as_primary(self):
+        self.known_subs = []
+        self.command_history = []
+        self.net.listen('/pleaseAddMe',
+                        self.handle_add_new_node)
+        self.net.listen('/pleaseExecuteThis',
+                        self.handle_execute_for_primary)
+
+    def handle_add_new_node(self,sub_endpoint: str,
+                            data: str) -> str:
+        """[For primary] to add new node"""
+        self.known_subs.append(sub_endpoint)
+        return f"""
+        Dear {sub_endpoint}
+            You are in. and here is what we have so far:
+            {''.join(self.command_history)}
+                   Sincerely
+                   {self.net.listened_endpoint()}, The primary.
+        """
+
+    def handle_execute_for_primary(self, endpoint: str,
+                                   data: str) -> str:
+        cmd = data
+        self.command_history.append(cmd)
+        self.exe.execute(cmd)
+        for sub in self.known_subs:
+            self.net.send(sub,'/pleaseExecuteThis',cmd)
+        return f"""
+        Dear {endpoint}
+             Your request has been carried out by our group.
+             Members: {self.known_subs}
+                 Sincerely
+                 {self.net.listened_endpoint()}, The primary.
+        """
+    def start_listening_as_sub(self):
+        self.net.listen('/pleaseExecuteThis',
+                        self.handle_execute_for_sub)
+    def handle_execute_for_sub(self,endpoint: str,data: str) -> str:
+        cmd = data
+        if (endpoint == self.primary):
+            self.exe.execute(cmd)
+            return f"""
+            Dear boss,
+                 Mission accomplished.
+                     Sincerely
+                     {self.net.listened_endpoint()}
+            """
+        else:
+            # forward
+            return self.net.send(self.primary,
+                                 '/pleaseExecuteThis',data)
