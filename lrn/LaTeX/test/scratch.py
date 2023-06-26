8568199900b4c732e4e81a6c5c7d342bd23d0905
Jianer <congjianer@outlook.com>
2023-06-26 15:06:40 +0800

    Start writing Raft

--- lrn/LaTeX/test/scratch.py
+++ lrn/LaTeX/test/scratch.py
@@ -18,6 +18,11 @@ class IStaticIdBasedNetworkable:
         # target: e.g. "/hi"
         pass
 
+import threading
+from threading import Thread, Timer
+import random
+# from random import randrange
+from time import sleep
 class RaftConsensus:
     def __init__(self,
                  n: IStaticIdBasedNetworkable,
@@ -27,5 +32,37 @@ class RaftConsensus:
 
         self.start_listening_as_follower()
     def start_listening_as_follower(self):
-        pass
+        patience = random.uniform(5,10)
+        print(f'node-{self.net.my_id()} has patience {patience}')
+        self.internal_clock = Timer(patience,function=have_enough,args=(self,))
 
+        self.net.listen('/pleaseAppendEntry', self.handle_append_entry)
+        self.net.listen('/pleaseVoteMe', self.handle_ask_for_vote)
+
+        self.internal_clock.start()
+
+
+    def have_enough(self):
+        print(f"node-{self.net.my_id()} has had enough")
+        self.ask_for_votes()
+
+    def handle_append_entry(self, i: int, d: str) -> Optional[str]:
+        self.comfort()
+        self.exe.execute(d)     # only primary will append entry
+        return f"""
+        Dear {i}, the primary
+             I have appended your requested entry.
+                    Yours {self.net.my_id()}
+        """
+
+    def comfort(self):          # reset timer
+        self.internal_clock.cancel()
+        patience = random.uniform(5,10)
+        print(f'node-{self.net.my_id()} has been consoled and got new patience {patience}')
+        self.internal_clock = Timer(patience,function=have_enough,args=(self,))
+        self.internal_clock.start()
+
+    def ask_for_votes():
+        pass
+    def handle_ask_for_vote():
+        pass
