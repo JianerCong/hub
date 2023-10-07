681d7380bf87a45c081b47ee6122bc30905d76e7
Jianer <congjianer@outlook.com>
2023-10-07 18:09:52 +0800

    Learn more ansible and openstack

--- lrn/ansible/os/03-conf-deploy.sh
+++ lrn/ansible/os/03-conf-deploy.sh
@@ -1,14 +1,3 @@
-# 1. add the public key to allow access (🦜: let me in)
-pk='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGhyRIycklSutg/Y8XsC6sMipaPXVfl7PpwEwZNA6h5t me@mypc'
-echo $pk >> /home/me/.ssh/authorized_keys
-
-# 2. set the http_proxy in /etc/environment --------------------------------------------------
-sudo bash -c "echo 'http_proxy=http://[fec0::1]:7890' >> /etc/environment"
-sudo bash -c "echo 'https_proxy=http://[fec0::1]:7890' >> /etc/environment"
-sudo bash -c "echo 'no_proxy=localhost' >> /etc/environment"
-
-# 🦜 : see conf-ipv6.sh
-
 
 # 🦜 : remember to add the
 #     - 'IP-CIDR6,fec1::/10,DIRECT'
@@ -36,7 +25,7 @@ deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe m
 f=/etc/apt/sources.list
 sudo bash -c "echo \"$x\" > $f"
 
-# 4. set the pip mirror 
+# 4. set the pip mirror
 x='[global]
 index-url = http://pip.example.org/simple'
 f=/root/.pydistutils.cfg
@@ -60,7 +49,7 @@ lxc_container_cache_files_from_host:
   - /etc/pip.conf
   - /root/.pydistutils.cfg
 lxc_ubuntu_mirror: \"{{ (ansible_facts['architecture'] == 'x86_64') | ternary('$m1', '$m2') }}\"
-lxc_apt_mirror: \"{{ (ansible_facts['distribution'] == 'Ubuntu') | ternary(lxc_ubuntu_mirror, '$m3') }}\" 
+lxc_apt_mirror: \"{{ (ansible_facts['distribution'] == 'Ubuntu') | ternary(lxc_ubuntu_mirror, '$m3') }}\"
 "
 f=/etc/openstack_deploy/user_variables.yml
 sudo mkdir /etc/openstack_deploy
--- lrn/ansible/os/m.sh
+++ lrn/ansible/os/m.sh
@@ -1,6 +1,8 @@
-ansible-playbook -i inv.yml pb.yml
+# ping and reboot
+ansible-playbook -i inv.yml p-hi.yml --become --become-pass-file ~/secret/my-pswd.txt
 
 
+# reboot all
 ansible -i inv.yml all \
         -m ansible.builtin.reboot -a '{"msg": "rebooting the host"}' \
         --become --become-password-file ~/secret/my-pswd.txt
